8a62837432dc67fffd3bf3b66ad23ab9088a603b
Joshua Jay Herman <30265+zitterbewegung@users.noreply.github.com>
2023-10-22 12:11:49 -0500

    Adding local support

---
+++ local_llm.py
@@ -0,0 +1,32 @@
+import torch
+from langchain import HuggingFacePipeline
+from transformers import AutoModelForCausalLM, AutoTokenizer, GenerationConfig, pipeline
+
+tokenizer = AutoTokenizer.from_pretrained("codellama/CodeLlama-13b-Instruct-hf")
+model = AutoModelForCausalLM.from_pretrained("codellama/CodeLlama-13b-Instruct-hf")
+
+MODEL_NAME = "codellama/CodeLlama-13b-Instruct-hf"
+ 
+#tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME, use_fast=True)
+ 
+#model = AutoModelForCausalLM.from_pretrained(
+#    MODEL_NAME,  trust_remote_code=True,
+#)
+tokenizer = tokenizer.to("cpu")
+model = model.to("cpu")
+ 
+generation_config = GenerationConfig.from_pretrained(MODEL_NAME)
+generation_config.max_new_tokens = 1024
+generation_config.temperature = 0.0001
+generation_config.top_p = 0.95
+generation_config.do_sample = True
+generation_config.repetition_penalty = 1.15
+ 
+text_pipeline = pipeline(
+    "text-generation",
+    model=model,
+    tokenizer=tokenizer,
+    generation_config=generation_config,
+)
+ 
+llm = HuggingFacePipeline(pipeline=text_pipeline, model_kwargs={"temperature": 0})
